54ff21189f80ce545e99d5813341e0ee05a58543
Umut Özkardeş <103787380+umutozkrds@users.noreply.github.com>
2025-01-26 16:09:00 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,17 +5,17 @@ import mysql.connector
 conn, mycursor = connection()
 
 
-def insert(): # burada kaldık
+def insert(): 
     name = input1.get()
     try:
-        old_price = int(input2.get())
-        new_price = int(input3.get())
+        old_price = 0
+        price = int(input2.get())
     except ValueError:
         print("Lütfen geçerli bir sayı girin!")
         return
 
     sql = "INSERT INTO products (name, eski_fiyat, yeni_fiyat) VALUES (%s, %s, %s)"
-    val= (name, old_price, new_price)
+    val= (name, old_price, price)
     mycursor.execute(sql, val)
     
     conn.commit()
@@ -23,7 +23,6 @@ def insert(): # burada kaldık
         print("Kayıt başarıyla eklendi!")
         input1.delete(0, END)
         input2.delete(0, END)
-        input3.delete(0, END)
         Label(root, text='Başarılı Kayıt').grid(row=5, column=0, padx=10, pady=10)
     else:
         print("Kayıt eklenemedi.")
@@ -42,21 +41,24 @@ def fetch_data():
     else:
         pass
 
+def clear_list():
+    listbox.delete(0,END)
+
+
+
+
 root = Tk(screenName=None, baseName=None, className="Tk", useTk=1)
 root.geometry("1000x500")
 
 Label(root, text='Ürün adı').grid(row=0, column=0, padx=10, pady=10)
-Label(root, text='Eski Fiyat').grid(row=1, column=0, padx=10, pady=10)
-Label(root, text='Yeni Fiyat').grid(row=2, column=0, padx=10, pady=10)
+Label(root, text='Fiyat').grid(row=1, column=0, padx=10, pady=10)
+
 
 input1 = Entry(root)
 input1.grid(row=0, column=1, padx=10, pady=10)
 
 input2 = Entry(root)
 input2.grid(row=1, column=1, padx=10, pady=10)
-
-input3 = Entry(root)
-input3.grid(row=2, column=1, padx=10, pady=10)
 
 button1 = Button(root, text="Kaydet", width=20, command=insert)
 button1.grid(row=3, column=0, columnspan=2,pady=10)
@@ -66,5 +68,8 @@ listbox.grid(column=1 , pady=20)
 
 fetch_button = Button(root, text="Verileri Getir", command=fetch_data)
 fetch_button.grid(column=1)
+
+clear_button = Button(root, text="Verileri Temizle", command=clear_list)
+clear_button.grid(row=7 ,column=1)
 
 root.mainloop()
